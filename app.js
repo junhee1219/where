204b8126465d4af685f58f1501b7afd411519d98
JHLee <leejunhee1219@snu.ac.kr>
2023-10-22 00:26:19 +0900

    https

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ createApp({
       this.result = '현재 위치를 찾는중입니다...';
       this.errorMessage = null;
       const api_key = "4c53494e5a74656c39326d6d684d54";
-      const url = `http://swopenAPI.seoul.go.kr/api/subway/${api_key}/json/realtimePosition/0/999/${this.selectedTile}`;
+      const url = `https://swopenAPI.seoul.go.kr/api/subway/${api_key}/json/realtimePosition/0/999/${this.selectedTile}`;
       var isSearch = false;
       //(1001:1호선, 1002:2호선, 1003:3호선, 1004:4호선, 1005:5호선 1006:6호선, 1007:7호선, 1008:8호선, 1009:9호선, 1063:경의중앙선,
       // 1065:공항철도, 1067:경춘선, 1075:수인분당선 1077:신분당선, 1092:우이신설선)    
